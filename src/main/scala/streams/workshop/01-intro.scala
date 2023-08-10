79bc3a28a15457de35d3c6062cc5744b54fd9047
Filipp Eremeev <floatfield@gmail.com>
2023-08-10 17:32:55 +0300

    intro done

--- src/main/scala/streams/workshop/01-intro.scala
+++ src/main/scala/streams/workshop/01-intro.scala
@@ -1,7 +1,7 @@
 package streams.workshop
 
 import zio._
-import zio.console.putStrLn
+import zio.console.{ getStrLn, putStrLn }
 import zio.duration._
 import java.nio.channels.AsynchronousFileChannel
 import java.nio.file.Paths
@@ -9,6 +9,11 @@ import java.nio.ByteBuffer
 import java.nio.channels.CompletionHandler
 import java.nio.channels.FileChannel
 import java.nio.file.Path
+import zio.clock.Clock
+import zio.blocking.Blocking
+import zio.console.Console
+import java.io.IOException
+import scala.io.Source
 
 object Types {
   // In ZIO, the `ZIO[R, E, A]` data type represents a program that
@@ -20,24 +25,24 @@ object Types {
   // correspond to the various ZIO signatures.
 
   // 1. A program that will succeed with an `A` or fail with an `E`.
-  type FailOrSuccess[E, A] = ???
+  type FailOrSuccess[E, A] = IO[E, A]
 
   // 2. A program that if it terminates, yields an `A`.
-  type Success[A] = ???
+  type Success[A] = UIO[A]
 
   // 3. A program that never terminates or fails.
-  type Forever = ???
+  type Forever = UIO[Nothing]
 
   // 4. A program that never terminates, but can fail with a throwable.
-  type MightThrow = ???
+  type MightThrow = IO[Throwable, Nothing]
 
   // 5. A program that requires access to a Clock, can fail with a throwable
   // and succeeds with an `A`.
-  type Program[A] = ???
+  type Program[A] = RIO[Clock, A]
 
   // 6. A program that requires access to blocking IO and the console,
   // could fail with a list of throwables or a number, and never terminates.
-  type Last = ???
+  type Last = ZIO[Blocking with Console, Either[List[Throwable], Int], Nothing]
 }
 
 object Values {
@@ -45,22 +50,32 @@ object Values {
   // we will survey the essential ZIO constructors.
 
   // 1. A program that succeeds with the string "Hello".
-  val hello: ??? = ???
+  val hello: UIO[String] = ZIO.succeed("Hello")
 
   // 2. A program that fails with the string "Boom".
-  val boom: ??? = ???
+  val boom: IO[String, Nothing] = ZIO.fail("Boom")
 
   // 3. A program that divides two numbers.
-  def div(x: Int, y: Int): ??? = ???
+  def div(x: Int, y: Int): IO[Throwable, Int] =
+    if (y == 0)
+      ZIO.fail(new Exception("Division by zero"))
+    else
+      ZIO.succeed(x / y)
 
   // 4. A program that prints out the requested line.
-  def printIt(line: String): ??? = ???
+  def printIt(line: String): ZIO[Console, IOException, Unit] = putStrLn(line)
 
   // 5. A program that reads a line from the console.
-  val readLine: ??? = ???
+  val readLine: ZIO[Console, IOException, String] = getStrLn
 
   // 6. A program that reads a file using blocking IO.
-  def readFile(name: String): ZIO[???, ???, String] = ???
+  def readFile(name: String): ZIO[Has[Blocking.Service], Throwable, String] =
+    blocking.effectBlocking {
+      val file     = Source.fromFile(name)
+      val contents = file.getLines()
+      file.close()
+      contents.mkString("\n")
+    }
 
   // 7. A program that executes the following side-effecting method,
   // and yields the results from its callbacks.
@@ -79,7 +94,9 @@ object Values {
     )
   }
 
-  def readFileZIO(name: String): IO[???, ???] = ???
+  def readFileZIO(name: String): IO[Throwable, Chunk[Byte]] =
+    ZIO.effectAsync(cb => readFileAsync(name, either => cb(ZIO.fromEither(either))))
+
 }
 
 // Topics: map, flatMap, zip, zipRight, zipPar, foreach, collect
@@ -90,7 +107,7 @@ object Sequencing {
 
   // 1. Use the `map` operator to convert any ZIO program that returns an
   // integer to a program that returns a string.
-  def toString[R, E](zio: ZIO[R, E, Int]): ZIO[R, E, String] = ???
+  def toString[R, E](zio: ZIO[R, E, Int]): ZIO[R, E, String] = zio.map(_.toString())
 
   // 2. Using a `for` comprehension, print a message for the user,
   // read a line, then print it out again.
@@ -170,26 +187,58 @@ object ManagedResources {
     }
   }
 
+  def readFileBytesZIO(path: String): ZIO[Blocking, Throwable, Chunk[Byte]] =
+    ZIO.bracket(
+      ZIO.effect(FileChannel.open(Paths.get(path))),
+      (channel: FileChannel) => ZIO.effect(if (channel ne null) channel.close()).orDie,
+      (channel: FileChannel) =>
+        blocking.effectBlocking {
+          val buf = ByteBuffer.allocate(8192)
+          channel.read(buf)
+          Chunk.fromByteBuffer(buf)
+        }
+    )
+
+  def writeFilesBytes(path: String, chunk: Chunk[Byte]): RIO[Blocking, Unit] =
+    ZIO.bracket(
+      ZIO.effect(FileChannel.open(Paths.get(path))),
+      (channel: FileChannel) => ZIO.effect(if (channel ne null) channel.close()).orDie,
+      (channel: FileChannel) =>
+        blocking.effectBlocking {
+          val _ = channel.write(ByteBuffer.wrap(chunk.toArray))
+        }
+    )
+
   // 2. Acquire channels to two files in a bracket and transfer a chunk of bytes between them:
-  val transfer: Task[Unit] = ???
+  val transfer: RIO[Blocking, Unit] = readFileBytesZIO("./file").flatMap(writeFilesBytes("./file2", _))
 
   // 3. Acquire channels to *three* files in a bracket and transfer a chunk
   // of bytes from the first to the second and third:
-  val transfer2: Task[Unit] = ???
+  val transfer2: RIO[Blocking, Unit] = readFileBytesZIO("./file").flatMap(channel =>
+    writeFilesBytes("./file2", channel) *> writeFilesBytes("./file3", channel)
+  )
 
   // 4. Create a ZManaged value that allocates a file channel.
-  def fileChannel(path: Path): ??? = ???
+  def fileChannel(path: Path): ZManaged[Any, Throwable, FileChannel] =
+    ZManaged.makeEffect(FileChannel.open(path))(_.close())
 
   // 5. Using `fileChannel`, acquire channels to all the requested paths:
-  def channels(paths: Path*): ??? = ???
+  def channels(paths: Path*): ZManaged[Any, Throwable, List[FileChannel]] =
+    ZManaged.foreach(paths)(fileChannel)
 
   // 6. Compose two managed file channels in a for-comprehension, and print
   // out a message after the second one is closed:
-  val channels2: ??? = ???
-
+  val channels2: ZManaged[Console, Throwable, (FileChannel, FileChannel)] = {
+// fileChannel()
+    for {
+      channel1 <- fileChannel(Paths.get("./some1"))
+      channel2 <- fileChannel(Paths.get("./some2")).ensuring(putStrLn("get it"))
+    } yield (channel1, channel2)
+  }
   // 7. Wrap this ZIO program with a fiber that prints out a message
   // every 5 seconds and is interrupted when the block ends:
-  val monitor: ??? = ZIO.sleep(10.seconds) ?
+  def monitor(program: Task[Unit]): ZIO[Clock with Console, Throwable, Unit] =
+    (ZIO.sleep(5.seconds) *> putStrLn("sss")).forever.race(program)
 
   // 8. Create a scope in which files can be safely opened and closed,
   // and write some data to 3 files in it:
@@ -197,7 +246,7 @@ object ManagedResources {
 
   // 9. Test your understanding of how ZManaged works by writing out
   // the order of prints in this snippet, without running it:
-  val ordering =
+  val ordering = // foo bar a b c c bin b bin a bin bar fin baz foo bin
     for {
       _ <- ZManaged.make(putStrLn("foo"))(_ => putStrLn("foo fin"))
       _ <- ZManaged.finalizer(putStrLn("baz"))
